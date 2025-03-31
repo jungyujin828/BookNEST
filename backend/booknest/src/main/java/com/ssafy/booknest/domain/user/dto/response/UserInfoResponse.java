c5485e15730aa7fa5f8629118a8caac8b06df043
이은지 <dmswldk28@gmail.com>
2025-03-31 09:19:10 +0900

    fix: 유저 Id 함께 넘김

--- backend/booknest/src/main/java/com/ssafy/booknest/domain/user/dto/response/UserInfoResponse.java
+++ backend/booknest/src/main/java/com/ssafy/booknest/domain/user/dto/response/UserInfoResponse.java
@@ -9,12 +9,13 @@ import lombok.Getter;
 @Getter
 @Builder
 public class UserInfoResponse {
+    private Integer userId;
     private String nickname;
     private Gender gender;
     private String birthDate;
     private String roadAddress;
     private String zipcode;
-//    private String profileURL;
+    private String profileURL;
     private Integer followers;
     private Integer followings;
     private Integer totalRatings;
@@ -22,10 +23,11 @@ public class UserInfoResponse {
 
     public static UserInfoResponse of(User user, Address address, Integer followers, Integer followings, Integer totalRatings, Integer totalReviews){
         return UserInfoResponse.builder()
+                .userId(user.getId())
                 .nickname(user.getNickname())
                 .gender(user.getGender())
                 .birthDate(user.getBirthdate())
-//                .profileURL(user.getProfileUrl())
+                .profileURL("https://res.cloudinary.com/gominsushi/image/upload/v1743145995/bird_xbfc1j.png")
                 .roadAddress(address.getRoadAddress())
                 .zipcode(address.getZipcode())
                 .followers(followers)
