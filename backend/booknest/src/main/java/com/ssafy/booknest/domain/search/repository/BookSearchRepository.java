53c649587fe0a47ecb7ae4bd4bba7a8b569933bf
이은지 <dmswldk28@gmail.com>
2025-04-06 23:58:36 +0900

    fix: 태그 검색 AND 수정

--- backend/booknest/src/main/java/com/ssafy/booknest/domain/search/repository/BookSearchRepository.java
+++ backend/booknest/src/main/java/com/ssafy/booknest/domain/search/repository/BookSearchRepository.java
@@ -3,41 +3,9 @@ package com.ssafy.booknest.domain.search.repository;
 import com.ssafy.booknest.domain.search.record.SearchedBook;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-import java.util.List;
 
-public interface BookSearchRepository extends ElasticsearchRepository<SearchedBook, String> {
+public interface BookSearchRepository extends ElasticsearchRepository<SearchedBook, Integer>, BookSearchCustomRepository {
     Page<SearchedBook> findByTitleContainingOrAuthorsContaining(String title, String authors, Pageable pageable);
-
-    @Query("""
-    {
-      "terms": {
-        "tags": ?0
-      }
-    }
-    """)
-    Page<SearchedBook> findByExactTags(List<String> tags, Pageable pageable);
-
-    @Query("""
-    {
-      "bool": {
-        "must": [
-          { "terms": { "tags": ?0 } },
-          {
-            "bool": {
-              "should": [
-                { "match": { "title": { "query": ?1 } } },
-                { "match": { "authors": { "query": ?1 } } }
-              ]
-            }
-          }
-        ]
-      }
-    }
-    """)
-    Page<SearchedBook> findByTagsAndKeyword(List<String> tags, String keyword, Pageable pageable);
-
-
 }
