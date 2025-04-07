341cb8973d547a932aec5330aed56876b008108e
이은지 <dmswldk28@gmail.com>
2025-04-07 13:54:13 +0900

    fix: 검색기능 최적화

--- backend/booknest/src/main/java/com/ssafy/booknest/domain/search/record/SearchedBook.java
+++ backend/booknest/src/main/java/com/ssafy/booknest/domain/search/record/SearchedBook.java
@@ -1,5 +1,6 @@
 package com.ssafy.booknest.domain.search.record;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,6 +18,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SearchedBook {
 
     @Id
